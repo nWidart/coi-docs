30c82811b6ce4b307df69043740f277a3be438c5
nwidart <n.widart@gmail.com>
2026-02-27 17:49:38 +0100

    Coverting docs to static site

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,9 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/docs/:path*.mdx',
-        destination: '/llms.mdx/docs/:path*',
-      },
-    ];
-  },
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
 };
 
 export default withMDX(config);
